39c6b74742227be9a2571bc7eb078e2c8efc63ef
itsannapanna <2022ugce078@nitjsr.ac.in>
2023-08-06 22:31:30 +0530

    ajk,jn

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function home() {
   return (
-    <div>home</div>
+    <><div>home</div>
+      <div class="flex items-center">
+        <input type="text" placeholder="search for a doc" class="py-2 px-4 border border-gray-300 rounded-1-md text-gray-700 ">
+          <img src="search_icon.png" alt="Search" class="w-5 h-5">
+          </img>
+        </input>
+      </div>
+    </>
   )
 }
