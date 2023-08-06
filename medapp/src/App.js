acd1108faf44f1b32c4c774f66398ff3290640c8
harshit960 <raj.harshit960@gmail.com>
2023-08-06 12:25:08 +0530

    router done

--- medapp/src/App.js
+++ medapp/src/App.js
@@ -1,29 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route ,Link} from "react-router-dom";
 import Home from "./component/Home";
+import NearBy from "./component/NearBy";
+import Notification from "./component/Notification";
+import DocProfile from "./component/DocProfile";
+import Reports from "./component/Reports";
+import Search from "./component/Search";
+import UserProfile from "./component/UserProfile";
+
 function App() {
   return (
-    <Router>
-        <div className="App">
-            <ul className="App-header">
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/about">About Us</Link>
-            </li>
-            <li>
-                <Link to="/contact">Contact Us</Link>
-            </li>
-            </ul>
-        <Routes>
-                <Route exact path='/' element={< Home />}></Route>
-                <Route exact path='/about' element={< About />}></Route>
-                <Route exact path='/contact' element={< Contact />}></Route>
-        </Routes>
-        </div>
-    </Router>
+    <><BrowserRouter>
+    
+      <Routes>
+                <Route index path='/' element={< Home />}/>
+                <Route  path='/nearby' element={< NearBy />}/>
+                <Route  path='/msg' element={< Notification />}/>
+                <Route  path='/doc' element={< DocProfile />}/>
+                <Route  path='/rep' element={< Reports />}/>
+                <Route  path='/search' element={< Search />}/>
+                <Route  path='/my' element={< UserProfile />}/>
+                </Routes>
+    </BrowserRouter>
+    </>
+
   );
 }
 
