a2c189b7b1c97d504cf891f7a304945d04628c76
harshit960 <raj.harshit960@gmail.com>
2023-08-08 20:23:53 +0530

    mui added +

--- src/component/DocProfile.js
+++ src/component/DocProfile.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import CallSharpIcon from '@mui/icons-material/CallSharp';
+import EmailIcon from '@mui/icons-material/Email';
 export default function docProfile() {
   return (
     <div>
@@ -11,14 +12,18 @@ export default function docProfile() {
           <img className=" w-9 h-9 left-0 top-0 absolute" src="https://via.placeholder.com/38x38" />
           <div className="text-white text-2xl font-bold">Dr. Name fd</div>
         </div>
-        <div className="w-28 h-12 left-[151px] top-[274px] absolute">
-          <div className="w-20 h-2.5 left-0 top-0 absolute text-neutral-700 text-xl font-medium">Dr. Name</div>
-          <div className="w-28 h-2 left-0 top-[25.61px] absolute text-neutral-700 text-base font-normal">Specialization</div>
-          <div className="w-28 h-2 left-0 top-[41.61px] absolute text-neutral-700 text-base font-normal">abouttttttttt</div>
-        </div>
         <img className="w-28 h-36 left-[19px] top-[200px] absolute rounded-2xl shadow" src="https://via.placeholder.com/110x142" />
-        <img className="w-7 h-7 left-[329px] top-[263px] absolute shadow" src="https://via.placeholder.com/30x30" />
-        <img className="w-7 h-7 left-[335px] top-[300px] absolute shadow" src="https://via.placeholder.com/30x30" />
+        <div className='absolute top-[280px] w-full flex justify-between'>
+          <div className="pl-36">
+            <div className="w-20  text-neutral-700 text-xl font-medium">Dr. Name</div>
+            <div className="w-28  text-neutral-700 text-base font-normal">Specialization</div>
+            <div className="w-28  text-neutral-700 text-base font-normal">abouttttttttt</div>
+          </div>
+          <div className="">
+            <CallSharpIcon className="w-7 h-7" />
+            <EmailIcon className="w-7 h-7" />
+          </div>
+        </div>
         <div className="w-96 h-96 left-[-52px] top-[360px] absolute">
           <div className="w-64 h-9 left-[71px] top-[87px] absolute text-black text-xl font-medium">Book the available slot</div>
           <div className="w-96 h-96 px-16 pt-32 pb-28 left-0 top-0 absolute flex-col justify-end items-center inline-flex">
