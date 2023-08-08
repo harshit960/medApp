728535ab631b7280afadcea0a39c201ad2742834
harshit960 <raj.harshit960@gmail.com>
2023-08-08 20:36:35 +0530

    doc

--- src/component/DocProfile.js
+++ src/component/DocProfile.js
@@ -8,8 +8,8 @@ export default function docProfile() {
         <div className="w-full h-96 left-0 top-[70px] absolute bg-white rounded-tl-3xl rounded-tr-3xl" >
           <img className="w-full h-48 rounded-tl-3xl rounded-tr-3xl" src="https://via.placeholder.com/390x193" />
         </div>
-        <div className="flex flex-row">
-          <img className=" w-9 h-9 left-0 top-0 absolute" src="https://via.placeholder.com/38x38" />
+        <div className="flex flex-row justify-center">
+          <img className=" w-9 h-9  absolute" src="https://via.placeholder.com/38x38" />
           <div className="text-white text-2xl font-bold">Dr. Name fd</div>
         </div>
         <img className="w-28 h-36 left-[19px] top-[200px] absolute rounded-2xl shadow" src="https://via.placeholder.com/110x142" />
@@ -19,9 +19,9 @@ export default function docProfile() {
             <div className="w-28  text-neutral-700 text-base font-normal">Specialization</div>
             <div className="w-28  text-neutral-700 text-base font-normal">abouttttttttt</div>
           </div>
-          <div className="">
-            <CallSharpIcon className="w-7 h-7" />
-            <EmailIcon className="w-7 h-7" />
+          <div className="grid grid-cols-1 pr-2">
+            <CallSharpIcon className="w-7 h-2" />
+            <EmailIcon className="w-7 h-2" />
           </div>
         </div>
         <div className="w-96 h-96 left-[-52px] top-[360px] absolute">
