97882000385c61560ed09e98ec02cb352a1c337c
harshit960 <raj.harshit960@gmail.com>
2023-08-08 18:44:31 +0530

    home draft done

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,13 +2,68 @@ import React from 'react'
 
 export default function home() {
   return (
-    <><div>home</div>
-      <div class="flex items-center">
-        <input type="text" placeholder="search for a doc" class="py-2 px-4 border border-gray-300 rounded-1-md text-gray-700 ">
-          <img src="search_icon.png" alt="Search" class="w-5 h-5">
-          </img>
-        </input>
+    <>
+
+      <div className="w-96 h-96 relative bg-zinc-100">
+        <div className="w-64 h-16 left-[19px] top-[18px] absolute">
+          <img className="w-16 h-16 left-0 top-0 absolute rounded-2xl border border-neutral-400" src="https://via.placeholder.com/65x63" />
+          <div className="w-44 h-6 left-[76px] top-[18px] absolute text-black text-xs font-normal">Good Morining</div>
+          <div className="w-28 h-7 left-[76px] top-[36px] absolute text-black text-xs font-medium">Hiria</div>
+        </div>
+        <div className="w-12 h-12 left-[313px] top-[24px] absolute">
+          <div className="w-12 h-12 left-0 top-0 absolute bg-white rounded-md border border-zinc-300" />
+        </div>
+        <div className="w-96 h-12 left-[24px] top-[104px] absolute">
+          <div className="w-96 h-12 left-0 top-0 absolute bg-white rounded-lg shadow" />
+          <img className="w-6 h-6 left-[10px] top-[12px] absolute opacity-70" src="https://via.placeholder.com/25x25" />
+          <div className="w-48 h-8 left-[44px] top-[17px] absolute text-black text-xs font-medium">Search for a doctor</div>
+        </div>
+        <div className="w-96 h-12 left-0 top-[794px] absolute">
+          <div className="w-96 h-12 left-0 top-0 absolute bg-white" />
+          <img className="w-9 h-9 left-[35px] top-[8px] absolute opacity-80 shadow" src="https://via.placeholder.com/35x36" />
+          <img className="w-9 h-9 left-[321px] top-[8px] absolute opacity-80 shadow" src="https://via.placeholder.com/35x36" />
+          <img className="w-9 h-9 left-[133px] top-[8px] absolute opacity-80 shadow" src="https://via.placeholder.com/35x36" />
+          <img className="w-9 h-9 left-[233px] top-[8px] absolute opacity-80 shadow" src="https://via.placeholder.com/35x36" />
+        </div>
+        <div className="w-96 h-48 left-[19px] top-[188px] absolute">
+          <div className="w-96 h-48 left-0 top-0 absolute bg-white rounded-md shadow" />
+          <img className="w-36 h-44 left-[16px] top-[14px] absolute rounded-lg" src="https://via.placeholder.com/151x169" />
+          <div className="w-40 h-6 left-[185px] top-[25px] absolute text-black text-2xl font-medium">Stay at Home!</div>
+          <div className="w-32 h-10 left-[190px] top-[63px] absolute text-black text-sm font-light">We are here to help you</div>
+          <div className="w-44 h-16 left-[176px] top-[113px] absolute bg-sky-500 rounded-lg" />
+          <div className="w-40 h-16 left-[176px] top-[125px] absolute text-center text-white text-base font-bold">BOOK AN APPOINTMENT</div>
+        </div>
+        <div className="w-80 h-14 left-[24px] top-[422px] absolute">
+          <div className="w-72 h-14 left-0 top-0 absolute text-neutral-700 text-2xl font-bold">Popular doctors</div>
+          <div className="w-14 h-5 left-[282px] top-[9px] absolute text-neutral-700 text-sm font-bold">See all</div>
+        </div>
+        <div className="w-60 h-40 left-[24px] top-[469px] absolute">
+          <div className="w-60 h-40 left-0 top-0 absolute bg-white rounded shadow" />
+          <img className="w-12 h-12 left-[8px] top-[12px] absolute rounded-full" src="https://via.placeholder.com/50x50" />
+          <div className="w-32 h-7 left-[62px] top-[19px] absolute text-black text-base font-bold">Dr. Somi</div>
+          <div className="w-32 h-5 left-[59px] top-[58px] absolute text-black text-sm font-thin">address </div>
+          <div className="w-32 h-5 left-[63px] top-[38px] absolute text-black text-xs font-extralight">Specialization</div>
+          <div className="w-56 h-11 left-[7px] top-[88px] absolute bg-sky-500 rounded-md" />
+          <div className="left-[18px] top-[102px] absolute text-white text-base font-bold">Select your slot and book</div>
+        </div>
+        <div className="w-60 h-40 left-[301px] top-[469px] absolute">
+          <div className="w-60 h-40 left-0 top-0 absolute bg-white rounded shadow" />
+          <img className="w-12 h-12 left-[8px] top-[12px] absolute rounded-full" src="https://via.placeholder.com/50x50" />
+          <div className="w-32 h-7 left-[62px] top-[19px] absolute text-black text-base font-bold">Dr. Somi</div>
+          <div className="w-32 h-5 left-[59px] top-[58px] absolute text-black text-sm font-thin">address </div>
+          <div className="w-32 h-5 left-[63px] top-[38px] absolute text-black text-xs font-extralight">Specialization</div>
+          <div className="w-56 h-11 left-[7px] top-[88px] absolute bg-sky-500 rounded-md" />
+          <div className="left-[18px] top-[102px] absolute text-white text-base font-bold">Select your slot and book</div>
+        </div>
+        <div className="w-80 h-32 left-[27px] top-[650px] absolute">
+          <div className="w-80 h-32 left-[1px] top-0 absolute bg-white rounded-lg shadow" />
+          <img className="w-32 h-32 left-[1px] top-[1px] absolute rounded-tl-lg" src="https://via.placeholder.com/123x124" />
+          <div className="w-80 h-12 left-0 top-[77px] absolute bg-sky-500 rounded-bl-lg rounded-br-lg" />
+          <div className="w-64 h-8 left-[40px] top-[89px] absolute text-white text-xl font-bold">Add your medical records</div>
+          <div className="w-48 h-11 left-[136px] top-[17px] absolute text-zinc-700 text-xl font-bold">Secure all yours medical data</div>
+        </div>
       </div>
+
     </>
   )
 }
